96db9f1f746e0fc3fe236ff52c3e585c155fad94
hantamil <mira0004@stud.kea.dk>
2023-11-13 13:09:25 +0100

    update

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,3 +1,15 @@
 public class Animal {
     private String name;
+
+    public abstract class makeSound{
+        private String sound;
+
+        public makeSound(String sound){
+            this.sound = sound;
+        }
+
+        public String getSound(){
+            return sound;
+        }
+    }
 }
